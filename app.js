f5df2f1e7ab92dbef9ecf565ab36e758aa2bc81c
Felipe Fernandez <heyitsfelipe@gmail.com>
2020-04-05 16:05:23 -0700

    Adds sample data and get route.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+const sampleResumeData = require('./sampleResumeData');
 
 const app = express();
+app.use(helmet());
+
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send(sampleResumeData);
+})
 
 app.listen(PORT, console.log(`Server started on PORT ${PORT}!  🎉`));
