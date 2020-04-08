0ff3055251a5fb060a0e44e29c7992eb9a17914e
Felipe Fernandez <heyitsfelipe@gmail.com>
2020-04-08 13:16:03 -0700

    Refactored, removed axios, added initial dashboard.

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
+
+const User = require('./models/User');
 
 const sampleResumeData = require('./sampleResumeData');
 const PORT = process.env.PORT || 3000;
@@ -29,9 +32,13 @@ app.get('/signup', (req, res) => {
     res.render('pages/signup');
 });
 
+app.get('/dashboard', (req, res) => {
+    res.render('pages/dashboard');
+});
+
 app.post('/signup', (req, res) => {
     console.log(">>>Sign Up Data", req.body);
-    res.send('Successfully Signed Up!');
+    res.redirect('/dashboard');
 });
 
 app.post('/login', (req, res) => {
