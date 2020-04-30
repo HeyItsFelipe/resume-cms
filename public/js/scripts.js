3547a8b4d2dfbee52916093ea2452fc487864a6f
Felipe Fernandez <heyitsfelipe@gmail.com>
2020-04-29 18:02:10 -0700

    Adds months/years dynamically and starting to add jobs on btn click.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -24,6 +24,19 @@ function validateSubmit(name, password) {
     return name.length > 5 && password.length > 5;
 }
 
+function startMonthOptions() {
+    const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let selectMonthDomEl = document.getElementsByClassName('start-month')[0];
+    if (selectMonthDomEl) {
+        for (let i = 0; i < MONTHS.length; i++) {
+            let optionEl = document.createElement("OPTION");
+            let optionText = document.createTextNode(MONTHS[i]);
+            optionEl.appendChild(optionText);
+            selectMonthDomEl.appendChild(optionEl);
+        }
+    }
+}
+
 function startYearOptions() {
     let selectYearDomEl = document.getElementsByClassName('start-year')[0];
     if (selectYearDomEl) {
@@ -37,8 +50,28 @@ function startYearOptions() {
     }
 }
 
+function addJobs() {
+    let addJobBtn = document.getElementsByClassName('add-job')[0];
+    let moreJobsDiv = document.getElementsByClassName('more-jobs')[0];
+    if (addJobBtn && moreJobsDiv) {
+        addJobBtn.addEventListener('click', () => {
+            let iDiv = document.createElement('div');
+            let jobToFill = `
+                <div class="input-field col s12">
+                    <label for="jobTitle">Title</label>
+                    <input type="text" id="jobTitle" name="jobTitle">
+                </div>
+            `;
+            iDiv.innerHTML = jobToFill;
+            moreJobsDiv.appendChild(iDiv);
+        });
+    }
+}
+
 setTimeout(() => {
     M.updateTextFields();
 }, 100);
 
+startMonthOptions();
 startYearOptions();
+addJobs();
