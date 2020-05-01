918e9f9d260e24664df477fe514a01a8d62e79a7
Felipe Fernandez <heyitsfelipe@gmail.com>
2020-04-30 19:32:03 -0700

    Modified how jobs are added.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -26,45 +26,74 @@ function validateSubmit(name, password) {
 
 function startMonthOptions() {
     const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let selectMonthDomEl = document.getElementsByClassName('start-month')[0];
+    let selectMonthDomEl = Array.from(document.getElementsByClassName('start-month'));
     if (selectMonthDomEl) {
-        for (let i = 0; i < MONTHS.length; i++) {
-            let optionEl = document.createElement("OPTION");
-            let optionText = document.createTextNode(MONTHS[i]);
-            optionEl.appendChild(optionText);
-            selectMonthDomEl.appendChild(optionEl);
-        }
+        selectMonthDomEl.forEach((el) => {
+            for (let i = 0; i < MONTHS.length; i++) {
+                let optionEl = document.createElement("OPTION");
+                let optionText = document.createTextNode(MONTHS[i]);
+                optionEl.appendChild(optionText);
+                el.appendChild(optionEl);
+            }
+        })
     }
 }
 
 function startYearOptions() {
-    let selectYearDomEl = document.getElementsByClassName('start-year')[0];
+    let selectYearDomEl = Array.from(document.getElementsByClassName('start-year'));
     if (selectYearDomEl) {
         let date = new Date;
-        for (let i = 1900; i <= date.getFullYear(); i++) {
-            let optionEl = document.createElement("OPTION");
-            let optionText = document.createTextNode(i);
-            optionEl.appendChild(optionText);
-            selectYearDomEl.appendChild(optionEl);
-        }
+        selectYearDomEl.forEach((el) => {
+            for (let i = 1900; i <= date.getFullYear(); i++) {
+                let optionEl = document.createElement("OPTION");
+                let optionText = document.createTextNode(i);
+                optionEl.appendChild(optionText);
+                el.appendChild(optionEl);
+            }
+        })
     }
 }
 
 function addJobs() {
-    let addJobBtn = document.getElementsByClassName('add-job')[0];
-    let moreJobsDiv = document.getElementsByClassName('more-jobs')[0];
-    if (addJobBtn && moreJobsDiv) {
-        addJobBtn.addEventListener('click', () => {
-            let iDiv = document.createElement('div');
-            let jobToFill = `
+    let jobDomEl = document.getElementById('jobs');
+    if (jobDomEl) {
+        let jobs = '';
+        for (let i = 1; i < 6; i++) {
+            let job = `
+                <div class="col s12"><h2>Job ${i}</h2></div>
                 <div class="input-field col s12">
                     <label for="jobTitle">Title</label>
                     <input type="text" id="jobTitle" name="jobTitle">
                 </div>
+                <div class="input-field col s12 m6">
+                    <label for="company">Company</label>
+                    <input type="text" id="company" name="company">
+                </div>
+                <div class="input-field col s12 m6">
+                    <label for="companyAddress">Company Address</label>
+                    <input type="text" id="companyAddress" name="companyAddress">
+                </div>
+                <div class="input-field col s12 m6">
+                    <select class="browser-default start-month">
+                        <option value="null-month" selected>Start Month</option>
+                    </select>
+                </div>
+                <div class="input-field col s12 m6">
+                    <select class="browser-default start-year">
+                        <option value="null-year" selected>Start Year</option>
+                    </select>
+                </div>
+                <div class="col s12" style="margin-top: 16px; margin-bottom: 16px;">
+                    <hr></hr>
+                </div>
             `;
-            iDiv.innerHTML = jobToFill;
-            moreJobsDiv.appendChild(iDiv);
-        });
+            jobs += job;
+        }
+        let div = document.createElement('DIV');
+        div.innerHTML = jobs;
+        jobDomEl.prepend(div);
+        startMonthOptions();
+        startYearOptions();
     }
 }
 
@@ -72,6 +101,4 @@ setTimeout(() => {
     M.updateTextFields();
 }, 100);
 
-startMonthOptions();
-startYearOptions();
 addJobs();
